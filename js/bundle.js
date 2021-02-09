8108615a1cc0401f1ef2ab4f3324028e3c00ebb9
Alina Khitrova <61740745+alina-kh@users.noreply.github.com>
2021-02-09 22:05:11 +0300

    Add files via upload

---
+++ js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+throw new Error("Module parse failed: Unexpected token (7:16)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n| import App from './components/app/';\n| \n> ReactDom.render(<App/>, document.getElementById('root'));\n| ");
+/******/ })()
+;
